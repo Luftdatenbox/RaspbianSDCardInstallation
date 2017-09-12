8b4650f878c2fd34b859c95a19e64934dfbd2e10
Gabriel Nikol <gabriel-martin.nikol@stud.uni-bamberg.de>
2017-09-12 23:02:48 +0200

    removed virtual env from docker-compose installation

--- Linux/scripts/installDockerCompose.sh
+++ Linux/scripts/installDockerCompose.sh
@@ -6,17 +6,17 @@ sudo apt install python-pip -y
 sudo pip install --upgrade pip
 
 # create virtualenv for docker-compose
-sudo pip install virtualenv
-mkdir ~/.docker-compose_virtualenv/
-cd ~/.docker-compose_virtualenv/
-virtualenv env
-source env/bin/activate
+#sudo pip install virtualenv
+#mkdir ~/.docker-compose_virtualenv/
+#cd ~/.docker-compose_virtualenv/
+#virtualenv env
+#source env/bin/activate
 
 # install docker-compose
 sudo pip install docker-compose
-deactivate
+#deactivate
 
 # set docker-compose as alias to the environment
-echo "alias docker-compose='~/.docker-compose_virtualenv/env/bin/docker-compose'" >> ~/.bashrc
+#echo "alias docker-compose='~/.docker-compose_virtualenv/env/bin/docker-compose'" >> ~/.bashrc
 
 
